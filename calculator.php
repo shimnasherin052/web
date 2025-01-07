d32430d94c5e988bbe72b0659754908b414d038b
shimnasherin052 <shimnasherin052@gmail.com>
2025-01-07 15:14:09 +0530

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,8 @@
+<?php
+$num1=10;
+$num2=20;
+echo "Addition:".($num1+$num2)."<br>";
+echo "subtraction:".($num1-$num2)."<br>";
+echo "Multiplication:".($num1*$num2)."<br>";
+echo "Division:".($num1/$num2)."<br>";
+?>
